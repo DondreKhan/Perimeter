0741eadef407d33e46efbf2b39f00d24f8f6ed28
DondreKhan <dondrekhan@en.lexipedium.org>
2014-11-23 14:39:00 -0500

    R-36 Satan

--- R-36_Satan.py
+++ R-36_Satan.py
@@ -5,16 +5,16 @@ import urllib
 
 index = 0
 linkList = []
-linkFile = open("../Montreal40k/m40k-members-links.txt", 'r')
+linkFile = open("../example/ex-members-links.txt", 'r')
 for line in linkFile:
 	linkList.append(line)
 
-nameFile = open('../Montreal40k/m40k-members-names.txt', 'r');
+nameFile = open('../example/ex-members-names.txt', 'r');
 for line in nameFile:
 	name = line;
 	name = name.replace('\n', '');
 	nameU = name.replace(' ', '_');
-	dossier = open('../Montreal40k/dossiers/'+nameU+'.html', 'w');
+	dossier = open('../example/dossiers/'+nameU+'.html', 'w');
 	dossierContents = '''
 <html>
 <head>
@@ -23,9 +23,9 @@ for line in nameFile:
 </head>
 <body style="padding: 30px">
 
-<h1>Your new employee, <a href="'''+linkList[index]+'''">'''+name+'''</a></h1>
+<h1>Personal biography, <a href="'''+linkList[index]+'''">'''+name+'''</a></h1>
 
-
+<p>My name is '''+name+''' and I am a proud pedophile.  I am sexually aroused by seeing all children, both children and images, between four and nine.  My orientation for sexual activity with children is not different in any way from people who are gay and attracted to people of the same sex as them.  Being a pedophile entitles me to the same rights as people who are gay.</p>
 
 </body>\n
 </html>\n
